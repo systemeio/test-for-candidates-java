e67ff5f82e6172fa3ff1c812024c90eff32608ae
Fuat_Safiulin <fuatsafiulin@gmail.com>
2024-07-11 14:11:38 +0100

    delete

--- src/payment/PaymentProcessor.java
+++
@@ -1,8 +0,0 @@
-package payment;
-
-public interface PaymentProcessor {
-
-    boolean payWithProcessor (double price) throws Exception;
-
-    String name();
-}
--- src/payment/StripePaymentProcessor.java
+++
@@ -1,9 +0,0 @@
-package payment;
-
-public class StripePaymentProcessor {
-
-    public boolean pay(Float price) {
-        return price >= 100;
-    }
-
-}
